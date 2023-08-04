94cc840853477a85390b1d53cd97a861dda84725
Chema Muñoz <chemunoz78@gmail.com>
2023-08-04 22:57:35 +0200

    feat: implement filtros on the component

--- src/app/todos/todo-footer/todo-footer.component.ts
+++ src/app/todos/todo-footer/todo-footer.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.state';
+import { filtrosValidos, setFiltro } from 'src/app/filtro/filtro.actions';
 
 @Component({
   selector: 'app-todo-footer',
   templateUrl: './todo-footer.component.html',
   styleUrls: ['./todo-footer.component.scss'],
 })
-export class TodoFooterComponent {}
+export class TodoFooterComponent implements OnInit {
+  public filtroActual: filtrosValidos = filtrosValidos.Todos;
+  public filtros: filtrosValidos[] = [
+    filtrosValidos.Todos,
+    filtrosValidos.Completados,
+    filtrosValidos.Pendientes,
+  ];
+
+  constructor(private store: Store<AppState>) {}
+
+  ngOnInit(): void {
+    this.store
+      .select('filtro')
+      .subscribe((filtro) => (this.filtroActual = filtro));
+  }
+
+  public cambiarFiltro(filtro: filtrosValidos): void {
+    this.store.dispatch(setFiltro({ filtro }));
+  }
+}
